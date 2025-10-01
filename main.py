4bcd0100f01fbb14caaebf043676eb4bcd812a2e
gngaganon <gngaganon@gmail.com>
2025-09-30 22:55:48 -0700

    Added Player

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 
 from constants import *
 
+from player import Player
+
 def main():
     # Initializing pygame
     pygame.init()
@@ -9,21 +11,28 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     # Creating pygame Clock object
     clock = pygame.time.Clock()
+    # Instantiating a Player object
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     # dt: "delta time"
     dt = 0
+    
+    # Game Loop
     while True:
         # Ensures GUI window buttons work: minimize, maximize, close
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # Dark background
         screen.fill("black")
+        # Player rotation (a and d keys)
+        player.update(dt)
+        # Rendering player
+        player.draw(screen)
+        # Screen flip
         pygame.display.flip()
         # Frame rate is set to 60 frames per second when .tick(60) is called
         # dt variable stores delta time
         dt = clock.tick(60)/1000
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
