d78c9e66f3b4f4650623f1dc48d839a2e8c169c2
gngaganon <gngaganon@gmail.com>
2025-10-04 20:12:08 -0700

    Set up containers; added asteroid field

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from constants import *
 
 from player import Player
 
+from asteroid import Asteroid
+
+from asteroidfield import AsteroidField
+
 def main():
     # Initializing pygame
     pygame.init()
@@ -11,11 +15,24 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     # Creating pygame Clock object
     clock = pygame.time.Clock()
+    # Creating containers
+    # updatable: objects that can be updated
+    # drawable: objects that can be drawn
+    # asteroids contain...asteroids
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    # Setting Player class in updatable and drawable
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     # Instantiating a Player object
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    # Instantiating an AsteroidField object
+    asteroid_field = AsteroidField()
     # dt: "delta time"
     dt = 0
-    
+
     # Game Loop
     while True:
         # Ensures GUI window buttons work: minimize, maximize, close
@@ -24,10 +41,11 @@ def main():
                 return
         # Dark background
         screen.fill("black")
-        # Player rotation (a and d keys)
-        player.update(dt)
-        # Rendering player
-        player.draw(screen)
+        # Player movement
+        updatable.update(dt)
+        # Rendering drawable objects
+        for drawing in drawable:
+            drawing.draw(screen)
         # Screen flip
         pygame.display.flip()
         # Frame rate is set to 60 frames per second when .tick(60) is called
